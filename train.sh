2acbcc1c324dcc6041d4ee6169a117aaf85b4d01
13482108753 <2225662681@qq.com>
2024-03-26 18:52:44 +0800

    first commit

---
+++ train.sh
@@ -0,0 +1,2 @@
+
+python /content/drive/MyDrive/SAM-Med3D-main/train.py  --multi_gpu --task_name adrenal_gland_left --checkpoint /content/drive/MyDrive/ckpt/sam_med3d.pth  --work_dir /content/drive/MyDrive/ckpt --gpu_ids 0  --num_epochs 50 --batch_size 32 --lr 0.001
---
+++ utils/data_paths.py
@@ -0,0 +1,11 @@
+img_datas = [
+'/content/drive/MyDrive/SAM-Med3D-main/segment_anything/data/train/adrenal_gland_left/ct',
+
+]
+
+all_classes = [
+'adrenal_gland_left',
+]
+
+all_datasets = ['ct',
+]
